45d8ca1945177c35d71d19415d1e31d868a624e9
arielhernandezcl <ariel.dox@gmail.com>
2024-07-26 22:25:07 -0400

    update command dolar

--- commands/dolar.js
+++ commands/dolar.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   async execute(interaction, client) {
     const DolarInsert = new EmbedBuilder().setAuthor({
-      name: `Te mostrare el valor del dolar en este minuto`,
+      name: `Consultando precio del dolar... `,
       iconURL: client.user.avatarURL()
     })
     const sent = await interaction.reply({
@@ -17,14 +17,12 @@ module.exports = {
       ephemeral: true
     })
 
-    const ValorDolar = await fetch('https://dolar.sonix.cl/')
-    const DolarData = await ValorDolar.json()
-    const CurrentPrice = DolarData[0].cp; 
-    const MinPrice = DolarData[0].mp.toFixed(2);
-    const MaxPrice = DolarData[0].hp;
-
+    const ValorDolar = await fetch('https://dolar.sonix.cl/');
+    const DolarData = await ValorDolar.json();
+    const CurrentPrice = DolarData.Dolares[0].Valor;
+    const Fecha = DolarData.Dolares[0].Fecha;
+    
     const DolarInsertBuild = new EmbedBuilder()
-
       .setAuthor({
         name: `Dolar USD/CLP - ${client.user.username} Chile`,
         iconURL: client.user.avatarURL()
@@ -33,19 +31,12 @@ module.exports = {
         {
           name: 'Dólar (Precio Actual)',
           value: `$${CurrentPrice}`,
-          //inline: true
         },
         {
-          name: 'Dólar (Precio Minimo)',
-          value: `$${MinPrice}`,
-          //inline: true
-        },
-        {
-          name: 'Dólar (Precio Maximo)',
-          value: `$${MaxPrice}`,
-          //inline: true
-        },
-      )
+          name: 'Fecha',
+          value: Fecha,
+        }
+      );
     await interaction.editReply({
       embeds: [DolarInsertBuild],
       ephemeral: true
